0c79dc7517368e00f69c0d1c63a28a0f54a36217
Alexandre Talles <alexandretalles777@gmail.com>
2022-12-16 19:33:24 -0300

    build server base

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { PrismaClient } from '@prisma/client'
 
 interface IUser {
+  id: number,
   name: string,
   email: string,
   password: string,
@@ -13,11 +14,19 @@ interface IApartament {
 }
 
 interface IOrder {
-
+  id: number,
+  status: 'Received' | 'Delivered',
+  apartamentNumber: number,
+  items: IItem[],
+  createdAt: Date,
+  updatedAt: Date,
 }
 
 interface IItem {
-
+  id: number,
+  orderId: number,
+  quantity: number,
+  name: string,
 }
 
 const prisma = new PrismaClient()
@@ -48,9 +57,8 @@ routes.post('/create-apartament', async (req, res) => {
 })
 
 
-
-
 //  USER
+
 routes.get("/get-users", async (req, res) => {
 
   const users = await prisma.user.findMany()
@@ -87,6 +95,44 @@ routes.post('/create-user', async (req, res) => {
 
     res.json(user)
 
+  }
+
+})
+
+
+// ORDER
+
+routes.post('/create-order', async (req, res) => {
+
+  const reqBody: IOrder = req.body
+
+  const {
+    apartamentNumber,
+    status,
+    items
+  } = reqBody
+
+  const apartamentNumberExists = await prisma.apartament.findUnique({
+    where: {
+      number: apartamentNumber
+    }
+  })
+
+  if (!apartamentNumberExists) {
+    return res.status(400).json({ error: 'Apartament number does not exists' })
+  }
+  else {
+    const order = await prisma.order.create({
+      data: {
+        status,
+        apartamentNumber,
+        itens: {
+          create: items
+        }
+      }
+    })
+
+    res.json(order)
   }
 
 })
